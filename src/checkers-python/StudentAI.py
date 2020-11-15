89f49037a04f18bd80396914954755560ad67a2e
Donneboe <59810298+Donneboe@users.noreply.github.com>
2020-11-14 18:21:22 -0800

    - Added a recursive depth-limited traversal - Added a Heuristics function - could definitely get improved later.

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -3,7 +3,7 @@ from BoardClasses import Move
 from BoardClasses import Board
 #The following part should be completed by students.
 #Students can modify anything except the class name and exisiting functions and varibles.
-class StudentAI():
+class StudentAI:
 
     def __init__(self,col,row,p):
         self.col = col
@@ -16,19 +16,63 @@ class StudentAI():
         self.color = 2
         # Add a timer to not exceed 8 minutes
 
-    # TODO : Future heuristic function
+    # TODO : improve this function
     # Possible Heuristics? :
-    # Enemy loses a normal piece/we lose a piece : +1 / -1
-    # Gaining a king/Taking enemy's king : +3
     # Losing a king/Enemy gains a king: -3
-    # we move with multiple jumps/Enemy moves with multiple jumps : +5 / -5 (if enemy has multiple jumps)
-    # Game Win / over : +100/-100
-    def heuristics(self):
-        return
+    def heuristics(self, old_diff, board, color):
+        diffInPieces = (board.white_count - board.black_count) if self.color == 1 else (
+                    board.black_count - board.white_count)
+        value = 0
+        difference = old_diff - diffInPieces
+        # There's no switch/case for python but I'm sure
+        # there's a better way for this part.
+        if difference > 1:
+            value += 5
+        elif difference < -1:
+            value -= 5
+        elif difference == 1:
+            value += 1
+        elif difference == -1:
+            value -= 1
 
-    # TODO : an actual tree traversal function for Monte Carlo Tree Search
-    def rTraversal(self, board):
-        return
+        # A tie is listed as a 0 as a return
+        # not sure what a tie's impact on the value should be.
+        winningNumber = board.is_win(color)
+        if winningNumber == color:
+            value += 100
+        elif winningNumber != color and winningNumber != 0:
+            value -= 100
+        return value
+
+    # Recursively
+    # board = a copy of the board after a move has been made.
+    # depth = Most likely need to limit the search else it will take too long computation wise
+    # color = if it's our turn, we choose most advantageous, else choose least advantageous
+    def r_traversal(self, board, depth, color):
+        # Prevents from going deeper to save time on calculation
+        if depth == 3:
+            return 0
+
+        moves = self.board.get_all_possible_moves(self.color)
+        cur_value = None
+
+        for pieces in moves:
+            for m in pieces:
+                board.make_move(m, self.color)
+                tmp = self.r_Traversal(board, depth + 1, self.opponent[color])
+                board.undo()
+                # Init the first move if not initialized.
+                if cur_value is None:
+                    cur_value = tmp
+                # If it's our turn, pick the higher value
+                if color == self.color:
+                    cur_value = tmp if tmp > cur_value else cur_value
+                # Else pick the lower one
+                else:
+                    cur_value = tmp if tmp < cur_value else cur_value
+        # This is in the case there is no possible moves, so curValue is left as None bc the
+        # loop does not run.
+        return 0 if cur_value is None else cur_value
 
     def get_move(self,move):
         # If a move has been made by the opponent, we are player 2
@@ -38,27 +82,27 @@ class StudentAI():
         else:
             self.color = 1
 
-        # Gets all the moves in the format : list of
+        # Gets all the possible moves in the format : list of
         # Ex : [ [(1,3)-(3,1)-(5,3)], [(4,5)-(3,4), (4,5)-(3,6)] ]
         moves = self.board.get_all_possible_moves(self.color)
 
-        # Temporary
-        move = 0
-        diffInPieces = 0;
+        #
+        move = None
+        curValue = None
         for pieces in moves:
-            for moves in pieces:
+            for m in pieces:
                 # Init move based on first/only move
-                if move == 0:
-                    move = moves
+                if move is None:
+                    move = m
                     self.board.make_move(move,self.color)
-                    diffInPieces = (self.board.white_count - self.board.black_count) if self.color == 1 else (self.board.black_count - self.board.white_count)
+                    curValue = self.r_traversal(self.board, 0, self.color)
                     self.board.undo()
                 else:
-                    self.board.make_move(moves,self.color)
-                    tmp = (self.board.white_count - self.board.black_count) if self.color == 1 else (self.board.black_count - self.board.white_count)
-                    if (tmp > diffInPieces):
-                        move = moves
-                        diffInPieces = tmp
+                    self.board.make_move(m,self.color)
+                    tmp = self.r_traversal(self.board, 0, self.color)
+                    if tmp > curValue:
+                        move = m
+                        curValue = tmp
                     self.board.undo()
 
         # Makes the move in our copy of the board
